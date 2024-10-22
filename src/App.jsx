df10a910eaaa847c039cea9a8ad673429da055e8
husna_tajrin <husnajahantajrin@gmail.com>
2024-10-22 13:02:09 +0600

    react knowledge cage setup added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-6xl font-semibold'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
